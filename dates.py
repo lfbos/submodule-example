45f7df1e56843eff57b16a3c5be4352762f62623
Luis Boscan <luis.boscan@globant.com>
2022-05-26 15:35:43 -0400

    Base files

---
+++ dates.py
@@ -0,0 +1,12 @@
+import pendulum
+
+def period_range(start_date: pendulum.DateTime, end_date: pendulum.DateTime, step_type: str = 'months', steps: int = 1):
+    if step_type not in ("years", "months", "weeks", "days", "hours", "minutes"):
+        raise ValueError("Invalid step_type must be one of: years, months, weeks, days, hours, minutes")
+
+    if steps <= 0:
+        raise ValueError("steps must be greater than zero")
+
+    period = end_date - start_date
+
+    return period.range(step_type, steps)
